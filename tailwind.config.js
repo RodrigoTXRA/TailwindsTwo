dd4a64f5ff38c8fbbe85ad56dfa1683cdf7d2eaa
Rodrigo Teixeira <rdgtxr@gmail.com>
2022-05-15 21:05:03 +1000

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,21 @@
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {
+      colors:{
+        primary:'#3cb371',
+        secondary:{
+          100: '#2e8b57',
+          200: '#228b22'
+        }
+      },
+      fontFamily:{
+        body:['Meie Script'],
+        bodyTwo:['Nunito']
+
+      },
+
+    },
+  },
+  plugins: [],
+}
